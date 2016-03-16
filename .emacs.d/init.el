d73cc0edd8cd2752524fa26e33b542932c594897
lsankar4033 <lsankar4033@gmail.com>
2016-03-15 18:35:12 -0700

    Add projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,7 +32,8 @@
     neotree
     ag
     fill-column-indicator
-    auto-complete))
+    auto-complete
+    projectile))
 
 ;; Set up exec-path-from-shell on osx
 (if (eq system-type 'darwin)
@@ -62,6 +63,12 @@
 ;; (evil-define-key 'normal magit-status-mode-map
 ;;  "j" 'magit-goto-next-section
 ;;  "k" 'magit-goto-previous-section) 
+
+;; Make it easy to reload this file
+(defun reload-init ()
+  (interactive)
+  (load-file "~/.emacs.d/init.el"))
+(evil-leader/set-key "l" 'reload-init) 
 
 ;; Auto-complete
 (ac-config-default)
@@ -100,24 +107,70 @@
 (load "setup-clojure.el")
 (load "setup-js.el")
 
+;; elisp bindings
+(evil-leader/set-key-for-mode 'emacs-lisp-mode
+  "e" 'eval-last-sexp)
+
 ;; Clojure specific bindings
 ;; cider refcard here: https://github.com/clojure-emacs/cider/blob/master/doc/cider-refcard.pdf
 ;; TODO cljs bindings
 (evil-leader/set-key-for-mode 'clojure-mode
   "c" 'cider-jack-in
   "e" 'cider-eval-last-sexp
-  "d" 'cider-doc
-  "f" 'cider-load-file)
+  "d" 'cider-doc)
 
-;; Org mode bindings
+;; Org mode todo bindings
 (evil-leader/set-key-for-mode 'org-mode
-  "t"  'org-show-todo-tree) 
+  "t" 'org-todo-list
+  "c" 'org-todo
+  "a" 'org-agenda-list) 
+
+;; Org mode timestamp bindings
+(evil-leader/set-key-for-mode 'org-mode
+  "s" 'org-schedule
+  "d" 'org-deadline
+  "j" 'org-timestamp-down-day
+  "k" 'org-timestamp-up-day)
 
 ;; Org mode TODO keywords
 (setq org-todo-keywords 
-      '((sequence "WAITING" "TODO" "|" "DONE" "DELEGATED")))
+      '((sequence "WAITING" "TODO" "IN PROGRESS" "|" "DONE" "DELEGATED")))
+
+;; Org mode agenda files 
+
+;; recursively find .org files in provided directory
+;; modified from an Emacs Lisp Intro example
+;; TODO move this into a utils file
+(defun sa-find-org-file-recursively (&optional directory filext)
+  "Return .org and .org_archive files recursively from DIRECTORY.
+If FILEXT is provided, return files with extension FILEXT instead." 
+  (let* (org-file-list
+	 (case-fold-search t)	      ; filesystems are case sensitive
+	 (file-name-regex "^[^.#].*") ; exclude dot, autosave, and backup files
+	 (filext (or filext "org$\\\|org_archive"))
+	 (fileregex (format "%s\\.\\(%s$\\)" file-name-regex filext))
+	 (cur-dir-list (directory-files directory t file-name-regex)))
+    ;; loop over directory listing
+    (dolist (file-or-dir cur-dir-list org-file-list) ; returns org-file-list
+      (cond
+       ((file-regular-p file-or-dir) ; regular files
+	(if (string-match fileregex file-or-dir) ; org files
+	    (add-to-list 'org-file-list file-or-dir)))
+       ((file-directory-p file-or-dir)
+	(dolist (org-file (sa-find-org-file-recursively file-or-dir filext)
+			  org-file-list) ; add files found to result
+	  (add-to-list 'org-file-list org-file)))))))
+
+;; NOTE this requires that emacs was run from iterm
+(defconst thought-docs-dir (concat (getenv "REPOS") "/thought-docs/"))
+(setq org-agenda-files
+      (sa-find-org-file-recursively thought-docs-dir))
 
 ;; Markdown mode
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; projectile
+(projectile-global-mode) 
+(evil-leader/set-key "b" 'projectile-find-file)
