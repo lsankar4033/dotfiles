da1f7a1ba3c216f0cede2586201cff045a31fb92
lsankar4033 <lsankar4033@gmail.com>
2016-07-12 21:20:30 +0200

    Update org-page emacs config

--- .emacs.d/elisp/org-page-personal.el
+++ .emacs.d/elisp/org-page-personal.el
@@ -1,7 +1,5 @@
-(provide 'org-page-personal)
-
-(setq op/repository-directory "~/src/personal-website")
-(setq op/site-domain "http://lakshmansankar.com")
+(setq op/repository-directory "~/src/personal-website/")
+(setq op/site-domain "http://lsankar.com/")
 ;; for commenting; disabled for now
 ;;(setq op/personal-disqus-shortname "your_disqus_shortname")
 
@@ -10,9 +8,30 @@
 
 (setq op/personal-github-link "https://github.com/lsankar4033")
 (setq op/site-main-title "Lakshman Sankar")
-;(setq op/site-sub-title "")
+(setq op/site-sub-title "Thoughts about the machine")
+(setq op/site-preview-directory "~/tmp/blog/")
 
-(evil-leader/set-key-for-mode 'org-mode
-  "pt" (progn
-	  (setq op/item-cache nil)
-	  (op/do-publication t "/tmp/blog" nil t nil)))
+(setq op/category-config-alist
+      '(("blog" ;; this is the default configuration
+	 :show-meta t
+	 :show-comment t
+	 :uri-generator op/generate-uri
+	 :uri-template "/blog/%y/%m/%d/%t/"
+	 :sort-by :date     ;; how to sort the posts
+	 :category-index t) ;; generate category index or not
+	("index"
+	 :show-meta nil
+	 :show-comment nil
+	 :uri-generator op/generate-uri
+	 :uri-template "/"
+	 :sort-by :date
+	 :category-index nil)
+	("about"
+	 :show-meta nil
+	 :show-comment nil
+	 :uri-generator op/generate-uri
+	 :uri-template "/about/"
+	 :sort-by :date
+	 :category-index nil)))
+
+(provide 'org-page-personal)
