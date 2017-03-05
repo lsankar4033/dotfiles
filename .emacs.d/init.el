652a52b4e8782b2d10b36b282180b8219624d33b
lsankar4033 <lsankar4033@gmail.com>
2017-03-05 09:24:44 +0530

    Remove garbage

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -308,18 +308,3 @@
 
 ;; less/css mode
 (setq css-indent-offset 2)
-
-;; Custom stuf,
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cider-cljs-lein-repl
-   "(do (require 'cljs.repl.node) (cemerick.piggieback/cljs-repl (cljs.repl.node/repl-env)))"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
